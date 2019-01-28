b806154bbaf68508932718e2c7cb6a8b32862bae
Chris Franklin <cf73021@gmail.com>
2019-01-28 21:29:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function takeANumber(current_line, new_person) {
   current_line.push(new_person)
+  var line_place = current_line.length
   return `Welcome, ${new_person}. You are number 10 in line.`
 }
