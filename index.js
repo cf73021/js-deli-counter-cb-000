49429cee8a0c54833b582bac90767d0c3e72f98e
Chris Franklin <cf73021@gmail.com>
2019-01-28 21:34:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber(current_line, new_person) {
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     return `Currently serving ${katzDeliLine[0]}.`
-    
+    katzDeliLine.shift()
   }
   else {
     return 'There is nobody waiting to be served!'}
