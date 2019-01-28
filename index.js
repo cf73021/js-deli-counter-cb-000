7094665fb832755a930db917b2c80f0dec80d6a0
Chris Franklin <cf73021@gmail.com>
2019-01-28 21:58:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function currentLine(current_line) {
       if (i = 0){
       line_string = line_string + `${i+1}. ` + `${current_line[i]}, `
       i++}
+      else if (i )
     }
     while (i < current_line.length)
   }
